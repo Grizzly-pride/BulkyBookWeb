d30c8eaa8bac370fc90f24b263bed886b7e36277
Alexander Medved <medved1991alexander@gmail.com>
2022-12-25 17:44:53 +0300

    Added repository implementation

--- BulkyBook.DataAccess/Repository/IRepositpry/IRepository.cs
+++ BulkyBook.DataAccess/Repository/IRepositpry/IRepository.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BulkyBook.DataAccess.Repository.IRepositpry
 {
-	internal interface IRepository
+	public interface IRepository<T> where T : class
 	{
-	}
+		//T - Category
+		T GetFirstOrDefault(Expression<Func<T, bool>> filter);
+		IEnumerable<T> GetAll();
+		void Add(T entity);
+		void Remove(T entity);
+		void RemoveRange(IEnumerable<T> entites);
+	}	
 }
