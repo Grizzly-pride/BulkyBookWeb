0711eeb9a5276e251531c2cc5807df191533c12a
Alexander Medved <medved1991alexander@gmail.com>
2023-02-20 11:27:51 +0300

    Extend Identity User

--- BulkyBook.DataAccess/Migrations/20230220082644_ExtendIdentityUser.Designer.cs
+++ BulkyBook.DataAccess/Migrations/20230220082644_ExtendIdentityUser.Designer.cs
@@ -4,16 +4,19 @@ using BulkyBook.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace BulkyBookWeb.Migrations
+namespace BulkyBook.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230220082644_ExtendIdentityUser")]
+    partial class ExtendIdentityUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
