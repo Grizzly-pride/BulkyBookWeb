5cefe2df36d6d179e5cfbe4276261e8409c6e93c
Alexander Medved <medved1991alexander@gmail.com>
2023-03-17 11:42:42 +0300

    Add shoping cart and application user repository

---
+++ BulkyBook.DataAccess/Repository/IRepositpry/IShoppingCartRepository.cs
@@ -0,0 +1,9 @@
+﻿using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+
+namespace BulkyBook.DataAccess.Repository.IRepositpry
+{
+	public interface IShoppingCartRepository : IRepository<ShoppingCart>
+	{
+	}
+}
