8d32d8f7888d2d8e9feae00b8719a0297edadcf9
Alexander Medved <medved1991alexander@gmail.com>
2023-05-08 14:26:27 +0300

    Add OrderDetail

--- BulkyBook.Models/OrderDetail.cs
+++ BulkyBook.Models/OrderDetail.cs
@@ -1,5 +1,23 @@
-﻿namespace BulkyBook.Models;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Models;
 
 public class OrderDetail
 {
+    public int Id { get; set; }
+    [Required]
+    public int OrderId { get; set; }
+    [ForeignKey("OrderId")]
+    [ValidateNever]
+    public OrderHeader OrderHeader { get; set; }
+
+    [Required]
+    public int ProductId { get; set; }
+    [ForeignKey("ProductId")]
+    [ValidateNever]
+    public Product Product { get; set; }
+    public int Count { get; set; }
+    public double Price { get; set; }
 }
