9332a277e4dd313f743af8d0f52ba708ae2d0c90
Alexander Medved <medved1991alexander@gmail.com>
2023-07-26 22:53:03 +0300

    Filters in Order List

--- BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,9 +1,13 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepositpry;
+using BulkyBook.Models.ViewModels;
+using BulkyBook.Utility;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
+using System.Diagnostics;
 
 namespace BulkyBookWeb.Areas.Admin.Controllers;
 
+[Area("admin")]
 public class OrderController : Controller
 {
     private readonly IUnitOfWork _unitOfWork;
@@ -18,12 +22,42 @@ public class OrderController : Controller
         return View();
     }
 
+    public IActionResult Details(int orderId)
+    {
+        OrderViewModel orderViewModel = new()
+        {
+            OrderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(filter: order => order.Id == orderId, includeProperties: new string[] { "ApplicationUser" }),
+            orderDetails = _unitOfWork.OrderDetail.GetAll(filter: i => i.OrderId == orderId, includeProperties: new string[] { "Product" })
+        };
+
+        return View(orderViewModel);
+    }
+
+
     #region API CALS
     [HttpGet]
-    public IActionResult GetAll()
+    public IActionResult GetAll(string status)
     {
         var orderList = _unitOfWork.OrderHeader
             .GetAll(includeProperties: new string[] { "ApplicationUser"});
+
+        switch (status)
+        {
+            case "pending":
+                orderList = orderList.Where(i => i.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                break;
+            case "inprocess":
+                orderList = orderList.Where(i => i.OrderStatus == SD.StatusInProcess);
+                break;
+            case "completed":
+                orderList = orderList.Where(i => i.OrderStatus == SD.StatusShipped);
+                break;
+            case "approved":
+                orderList = orderList.Where(i => i.OrderStatus == SD.StatusApproved);
+                break;
+            default:
+                break;
+        }
 
         return Json(new { data = orderList });
     }
